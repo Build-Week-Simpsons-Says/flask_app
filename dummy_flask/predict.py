d748f5cd68b421d598e0144fde7a9619f54e7391
seanantosiak <seanantosiak@gmail.com>
2019-10-23 14:56:54 -0400

    updated from local repo

--- dummy_flask/predict.py
+++ dummy_flask/predict.py
@@ -1,29 +1,15 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import NearestNeighbors
+from joblib import load
 
 # load dataframe
 df = pd.read_csv('filtered_dataset.csv')
 
-# creates vectorizer
-tfidf = TfidfVectorizer(stop_words='english',
-                        min_df=0.01,
-                        max_df=0.99,
-                        ngram_range=(1, 3))
+# loads pickled TfidfVectorizer model
+tfidf = load('tfidf.joblib')
 
-# creates sparse matrix of lemmas
-sparse = tfidf.fit_transform(df['spoken_words'])
-
-# creates dense matrix from sparse matrix
-dense = sparse.todense()
-
-# creates dataframe from dense matrix
-dense_df = pd.DataFrame(dense, columns=tfidf.get_feature_names())
-
-# creates NearestNeighbors model and fits it to dense dataframe
-nn_model = NearestNeighbors(n_neighbors=2, algorithm='kd_tree')
-nn_model.fit(dense_df)
+# loads pickled nearest neighbor model
+nn_model = load('nn_model.joblib')
 
 
 def get_quote(input_text):
@@ -50,8 +36,14 @@ def get_quote(input_text):
         # creates vector of input text
         quote_vec = tfidf.transform([input_text])
 
-        # gets the index for the most similar quote to the input text
-        similar_index = nn_model.kneighbors(quote_vec.todense())[1][0][0]
+        # gets an object that contains the index values for similar quotes
+        similar_quotes = nn_model.kneighbors(quote_vec.todense())[1][0]
+
+        # generates a number to select out of the most similar quotes at random
+        i = np.random.randint(len(similar_quotes))
+
+        # gets the index value for the selected quote
+        similar_index = similar_quotes[i]
 
         # gets the quote from the dataframe and returns it
         quote_row = df.iloc[similar_index]
